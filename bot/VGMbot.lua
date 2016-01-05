8219c9e6b9d41b5fd628ca91310796ee57598821
alireza1998 <alireza.eidani1000@gmail.com>
2016-01-06 01:53:51 +0330

    Update VGMbot.lua

--- bot/VGMbot.lua
+++ bot/VGMbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {172631426,159887854,0,tonumber(our_id)},--Sudo users
+    sudo_users = {116065091,159887854,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
