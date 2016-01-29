d89c855bd11199170555a3526ee6517b8ec57e15
githubmereza <github09355332076@gmail.com>
2016-01-29 12:19:20 +0330

    Update VGMbot.lua

--- bot/VGMbot.lua
+++ bot/VGMbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "qr",
     "plugins"
     },
-    sudo_users = {151624528,171481453,0,tonumber(our_id)},--Sudo users
+    sudo_users = {170774776,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
